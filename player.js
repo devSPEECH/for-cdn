4c11aa1464f0a34cffbf2dcf333a31f919e1fb6a
Amine Kh <89208592+devSPEECH@users.noreply.github.com>
2023-07-04 12:03:42 +0100

    Update player.js

--- player.js
+++ player.js
@@ -5468,7 +5468,7 @@ if (typeof already_executed === 'undefined') {
     }
     let minimizedMode = false;
     let autoScroll = true;
-    let doubleClick = false;
+    let doubleClick = true;
     let webMonetization = false;
     let playOnClick = false;
 
