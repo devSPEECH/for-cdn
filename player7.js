0307a82b2298a30e96cf0b855e2f8f306b3d2c08
Amine Kh <89208592+devSPEECH@users.noreply.github.com>
2023-08-03 14:23:53 +0100

    Update and rename player6.js to player7.js

--- player7.js
+++ player7.js
@@ -4967,7 +4967,7 @@ if (typeof already_executed === 'undefined') {
             const mediaFragment = this.getSelectionMediaFragment();
 
             if (mediaFragment !== '') {
-              //document.location.hash = mediaFragment;
+              document.location.hash = mediaFragment;
             }
           },
           false,
@@ -5037,8 +5037,8 @@ if (typeof already_executed === 'undefined') {
           playHeadEvent = 'dblclick';
         }
 
-        this.transcript.addEventListener(playHeadEvent, this.setPlayHead, false);
-        this.transcript.addEventListener(playHeadEvent, this.checkPlayHead, false);
+        //this.transcript.addEventListener(playHeadEvent, this.setPlayHead, false);
+        //this.transcript.addEventListener(playHeadEvent, this.checkPlayHead, false);
 
         this.start = this.hashArray[0];
 
