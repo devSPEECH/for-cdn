a66c3443a1d875197ca3e29b5d99b535eb238931
Amine Kh <89208592+devSPEECH@users.noreply.github.com>
2023-08-15 18:28:29 +0100

    Update and rename player13.js to player14.js

--- player14.js
+++ player14.js
@@ -4979,7 +4979,7 @@ if (typeof already_executed === 'undefined') {
         this.autoscroll = autoscroll;
         this.scrollerContainer = this.transcript;
         this.scrollerOffset = 0;
-        this.scrollerDuration = 200;
+        this.scrollerDuration = 2000;
         this.scrollerDelay = 0;
 
         this.doubleClick = doubleClick;
